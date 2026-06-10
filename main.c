d6b242d9d2703927c8e1301f6e9cac91bfe509ec
sanjanareddy279-crypto <sanjanasweety279@gmail.com>
2026-06-10 17:19:38 +0530

    Added interactive menu driven graphics editor

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ void initializeCanvas()
     {
         for(int j=0;j<COLS;j++)
         {
-            canvas[i][j] = '_';
+            canvas[i][j]='_';
         }
     }
 }
@@ -24,69 +24,79 @@ void displayCanvas()
     {
         for(int j=0;j<COLS;j++)
         {
-            printf("%c", canvas[i][j]);
+            printf("%c",canvas[i][j]);
         }
         printf("\n");
     }
 }
+
 void drawRectangle(int x,int y,int width,int height)
 {
     for(int j=x;j<x+width;j++)
     {
-        canvas[y][j]='*';
-        canvas[y+height-1][j]='*';
+        if(j<COLS)
+        {
+            canvas[y][j]='*';
+            canvas[y+height-1][j]='*';
+        }
     }
 
     for(int i=y;i<y+height;i++)
     {
-        canvas[i][x]='*';
-        canvas[i][x+width-1]='*';
+        if(i<ROWS)
+        {
+            canvas[i][x]='*';
+            canvas[i][x+width-1]='*';
+        }
     }
 }
+
 void drawLine(int x1,int y1,int x2,int y2)
 {
-    int dx = abs(x2 - x1);
-    int dy = abs(y2 - y1);
+    int dx=abs(x2-x1);
+    int dy=abs(y2-y1);
 
-    int steps = (dx > dy) ? dx : dy;
+    int steps=(dx>dy)?dx:dy;
 
-    float xInc = (float)(x2 - x1) / steps;
-    float yInc = (float)(y2 - y1) / steps;
+    float xInc=(float)(x2-x1)/steps;
+    float yInc=(float)(y2-y1)/steps;
 
-    float x = x1;
-    float y = y1;
+    float x=x1;
+    float y=y1;
 
     for(int i=0;i<=steps;i++)
     {
-        if((int)x >= 0 && (int)x < COLS &&
-           (int)y >= 0 && (int)y < ROWS)
+        if((int)x>=0 && (int)x<COLS &&
+           (int)y>=0 && (int)y<ROWS)
         {
-            canvas[(int)y][(int)x] = '*';
+            canvas[(int)y][(int)x]='*';
         }
 
-        x += xInc;
-        y += yInc;
+        x+=xInc;
+        y+=yInc;
     }
 }
+
 void drawTriangle(int x1,int y1,int x2,int y2,int x3,int y3)
 {
     drawLine(x1,y1,x2,y2);
     drawLine(x2,y2,x3,y3);
     drawLine(x3,y3,x1,y1);
 }
+
 void drawCircle(int cx,int cy,int radius)
 {
-    for(int angle=0; angle<360; angle++)
+    for(int angle=0;angle<360;angle++)
     {
-        double rad = angle * 3.14159 / 180.0;
+        double rad=angle*3.14159/180.0;
 
-        int x = cx + radius * cos(rad);
-        int y = cy + radius * sin(rad);
+        int x=cx+radius*cos(rad);
+        int y=cy+radius*sin(rad);
 
-        if(x >= 0 && x < COLS &&
-           y >= 0 && y < ROWS)
+        if(x>=0 && x<COLS &&
+           y>=0 && y<ROWS)
         {
-            canvas[y][x] = '*';
+            canvas[y][x]='*';
         }
     }
 }
@@ -112,30 +122,91 @@ int main()
         switch(choice)
         {
             case 1:
-                drawRectangle(5,5,10,5);
+            {
+                int x,y,width,height;
+
+                printf("Enter x: ");
+                scanf("%d",&x);
+
+                printf("Enter y: ");
+                scanf("%d",&y);
+
+                printf("Enter width: ");
+                scanf("%d",&width);
+
+                printf("Enter height: ");
+                scanf("%d",&height);
+
+                drawRectangle(x,y,width,height);
                 displayCanvas();
                 break;
+            }
 
             case 2:
-                drawCircle(20,10,5);
+            {
+                int cx,cy,r;
+
+                printf("Enter center x: ");
+                scanf("%d",&cx);
+
+                printf("Enter center y: ");
+                scanf("%d",&cy);
+
+                printf("Enter radius: ");
+                scanf("%d",&r);
+
+                drawCircle(cx,cy,r);
                 displayCanvas();
                 break;
+            }
 
             case 3:
-                drawLine(2,2,20,15);
+            {
+                int x1,y1,x2,y2;
+
+                printf("Enter x1: ");
+                scanf("%d",&x1);
+
+                printf("Enter y1: ");
+                scanf("%d",&y1);
+
+                printf("Enter x2: ");
+                scanf("%d",&x2);
+
+                printf("Enter y2: ");
+                scanf("%d",&y2);
+
+                drawLine(x1,y1,x2,y2);
                 displayCanvas();
                 break;
+            }
 
             case 4:
-                drawTriangle(5,5,20,5,12,15);
+            {
+                int x1,y1,x2,y2,x3,y3;
+
+                printf("Enter x1 y1: ");
+                scanf("%d%d",&x1,&y1);
+
+                printf("Enter x2 y2: ");
+                scanf("%d%d",&x2,&y2);
+
+                printf("Enter x3 y3: ");
+                scanf("%d%d",&x3,&y3);
+
+                drawTriangle(x1,y1,x2,y2,x3,y3);
                 displayCanvas();
                 break;
+            }
 
             case 5:
+                printf("Exiting...\n");
                 return 0;
 
             default:
                 printf("Invalid Choice\n");
         }
     }
+
+    return 0;
 }
